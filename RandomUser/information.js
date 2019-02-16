c17bed4bb3679228e594bf5a989707d1df6a286d
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-02-16 16:41:59 -0500

    Informacion de usuario

--- RandomUser/information.js
+++ RandomUser/information.js
@@ -1,11 +1,17 @@
 let usuarioString = localStorage.getItem("usuario");
 let objeto = JSON.parse(usuarioString);
-
+console.log(objeto);
 let container = document.querySelector(".container");
-let name = document.createElement("H2");
-name.innerText = objeto.name.first;
-container.appendChild(name);
+let information = document.createElement("p");
+information.className = "information";
+information.innerHTML = `
+  <img src=${objeto.picture.medium}>
+  <strong>Name: </strong><p>${objeto.name.first} ${objeto.name.last}</p>
+  <strong>Phone: </strong><p>${objeto.phone}</p>
+   <strong>City: </strong><p>${objeto.location.city}</p>
+   <strong>Email: </strong><p>${objeto.email}</p>
+    <strong>Age: </strong><p>${objeto.dob.age}</p>
+    <strong>Gender: </strong><p>${objeto.gender}</p>
+`;
 
-let email = document.createElement("P");
-email.innerText = objeto.email;
-container.appendChild(email);
+container.appendChild(information);
