128c076e5171ac80acb9a3cf5dfb13beb5f33bf0
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-02-17 12:24:22 -0500

    Estilos

--- RandomUser/information.js
+++ RandomUser/information.js
@@ -1,17 +1,21 @@
-let usuarioString = localStorage.getItem("usuario");
-let objeto = JSON.parse(usuarioString);
+const usuarioString = localStorage.getItem("usuario");
+const objeto = JSON.parse(usuarioString);
 console.log(objeto);
-let container = document.querySelector(".container");
-let information = document.createElement("p");
+const container = document.querySelector(".container");
+const information = document.createElement("p");
 information.className = "information";
+const picture = document.createElement("IMG");
+picture.setAttribute("src", objeto.picture.medium);
+picture.className = "imgUser";
+container.appendChild(picture);
+
 information.innerHTML = `
-  <img src=${objeto.picture.medium}>
-  <strong>Name: </strong><p>${objeto.name.first} ${objeto.name.last}</p>
-  <strong>Phone: </strong><p>${objeto.phone}</p>
-   <strong>City: </strong><p>${objeto.location.city}</p>
-   <strong>Email: </strong><p>${objeto.email}</p>
-    <strong>Age: </strong><p>${objeto.dob.age}</p>
-    <strong>Gender: </strong><p>${objeto.gender}</p>
+  <strong>Name: </strong>${objeto.name.first} ${objeto.name.last}<br />
+  <strong>Phone: </strong>${objeto.phone}<br />
+   <strong>City: </strong>${objeto.location.city}<br />
+   <strong>Email: </strong>${objeto.email}<br />
+    <strong>Age: </strong>${objeto.dob.age}<br />
+    <strong>Gender: </strong>${objeto.gender}<br />
 `;
 
 container.appendChild(information);
